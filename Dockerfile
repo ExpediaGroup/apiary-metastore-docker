d2496cb3e3a8da17d362ad7490694610f980489b
rpoluri <38321430+rpoluri@users.noreply.github.com>
2018-08-22 07:52:55 -0500

    Configure hive metastore ranger authorization (#26)
    * fixes #25 cofigure hive metastore ranger authorization
    
    * option to send ranger audit logs to mysql database
    
    * ApiaryRangerAuthPreEventListener misc fixes
    
    merging the change for now, will move code to apiary-extensions repo once that is ready.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@
 from amazonlinux:latest
 
 ENV VAULT_VERSION 0.10.3
+ENV RANGER_VERSION 1.1.0
 
 COPY files/RPM-GPG-KEY-emr /etc/pki/rpm-gpg/RPM-GPG-KEY-emr
 COPY files/emr-apps.repo /etc/yum.repos.d/emr-apps.repo
@@ -16,6 +17,16 @@ RUN yum -y install emrfs
 
 RUN wget -qN https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip && unzip -q -o vault_${VAULT_VERSION}_linux_amd64.zip -d /usr/local/bin/ && rm -f vault_${VAULT_VERSION}_linux_amd64.zip
 
+RUN cd /usr/lib/hive/lib/ && \
+wget -qN http://search.maven.org/remotecontent?filepath=org/apache/ranger/ranger-plugins-audit/${RANGER_VERSION}/ranger-plugins-audit-${RANGER_VERSION}.jar && \
+wget -qN http://search.maven.org/remotecontent?filepath=org/apache/ranger/ranger-plugins-common/${RANGER_VERSION}/ranger-plugins-common-${RANGER_VERSION}.jar && \
+wget -qN http://search.maven.org/remotecontent?filepath=org/apache/ranger/ranger-plugins-cred/${RANGER_VERSION}/ranger-plugins-cred-${RANGER_VERSION}.jar && \
+wget -qN https://search.maven.org/remotecontent?filepath=org/apache/solr/solr-solrj/5.5.4/solr-solrj-5.5.4.jar && \
+wget -qN https://search.maven.org/remotecontent?filepath=org/apache/httpcomponents/httpmime/4.5.5/httpmime-4.5.5.jar && \
+wget -qN https://search.maven.org/remotecontent?filepath=org/noggit/noggit/0.8/noggit-0.8.jar && \
+wget -qN https://search.maven.org/remotecontent?filepath=javax/persistence/javax.persistence-api/2.2/javax.persistence-api-2.2.jar && \
+wget -qN https://search.maven.org/remotecontent?filepath=org/eclipse/persistence/eclipselink/2.7.3/eclipselink-2.7.3.jar
+
 COPY src /src
 RUN cd src && javac -cp "/usr/lib/hadoop/*:/usr/lib/hive/lib/*:/usr/share/aws/aws-java-sdk/*" *.java && jar cf /usr/lib/hive/lib/MetastoreListeners.jar *.class && rm -f *.class
 
@@ -23,6 +34,8 @@ RUN echo 'export HADOOP_CLASSPATH="$HADOOP_CLASSPATH:/usr/share/aws/emr/emrfs/co
 COPY files/core-site.xml /etc/hadoop/conf/core-site.xml
 COPY files/emrfs-site.xml /usr/share/aws/emr/emrfs/conf/emrfs-site.xml
 COPY files/hive-site.xml /etc/hive/conf/hive-site.xml
+COPY files/ranger-hive-security.xml /etc/hive/conf/ranger-hive-security.xml
+COPY files/ranger-hive-audit.xml /etc/hive/conf/ranger-hive-audit.xml
 
 EXPOSE 9083
 COPY files/startup.sh /startup.sh
--- files/startup.sh
+++ files/startup.sh
@@ -13,6 +13,37 @@ else
     dbpass=`vault read -field=password ${vault_path}/hive_rouser`
 fi
 
+#configure LDAP group mapping, required for ranger authorization
+if [[ -n $LDAP_URL ]] ; then
+    sed "s/LDAP_BIND_USER/$(vault read -field=bind_user ${vault_path}/ldap_user)/" -i /etc/hadoop/conf/core-site.xml
+    vault read -field=bind_password ${vault_path}/ldap_user > /etc/hadoop/conf/ldap-password.txt
+    sed 's/org.apache.hadoop.security.JniBasedUnixGroupsMappingWithFallback/org.apache.hadoop.security.LdapGroupsMapping/' -i /etc/hadoop/conf/core-site.xml
+    sed "s/LDAP_URL/${LDAP_URL}/" -i /etc/hadoop/conf/core-site.xml
+    sed "s/LDAP_BASE/${LDAP_BASE}/" -i /etc/hadoop/conf/core-site.xml
+    #configure local ca certificate to connect o ldap/ad
+    vault read -field=cacert ${vault_path}/ldap_user > /etc/pki/ca-trust/source/anchors/ldapca.crt
+    update-ca-trust
+fi
+
+#configure ranger authorization
+if [[ -n $POLICY_MGR_URL ]]; then
+    export METASTORE_PRELISTENERS="${METASTORE_PRELISTENERS},ApiaryRangerAuthPreEventListener"
+    sed "s/POLICY_MGR_URL/${POLICY_MGR_URL}/" -i /etc/hive/conf/ranger-hive-security.xml
+    sed "s/RANGER_SERVICE_NAME/${RANGER_SERVICE_NAME}/" -i /etc/hive/conf/ranger-hive-security.xml
+fi
+#enable ranger auditing
+if [[ -n $AUDIT_SOLR_URL ]]; then
+    sed -e '/xasecure.audit.is.enabled/ { n; s/false/true/ }' -e '/xasecure.audit.solr.is.enabled/ { n; s/false/true/ }' -i /etc/hive/conf/ranger-hive-audit.xml
+    sed "s/AUDIT_SOLR_URL/${AUDIT_SOLR_URL}/" -i /etc/hive/conf/ranger-hive-audit.xml
+fi
+#enable ranger db auditing
+if [[ -n $AUDIT_DB_URL ]]; then
+    sed -e '/xasecure.audit.is.enabled/ { n; s/false/true/ }' -e '/xasecure.audit.db.is.enabled/ { n; s/false/true/ }' -i /etc/hive/conf/ranger-hive-audit.xml
+    sed "s/AUDIT_DB_URL/${AUDIT_DB_URL}/" -i /etc/hive/conf/ranger-hive-audit.xml
+    sed "s/AUDIT_DB_USER/$(vault read -field=username ${vault_path}/audit_db_user)/" -i /etc/hive/conf/ranger-hive-audit.xml
+    sed "s/AUDIT_DB_PASSWORD/$(vault read -field=password ${vault_path}/audit_db_user)/" -i /etc/hive/conf/ranger-hive-audit.xml
+fi
+
 #check if database is initialized, test only from rw instances
 if [ x"$instance_type" = x"readwrite" ]; then
 MYSQL_OPTIONS="-h$dbhost -u$dbuser -p$dbpass $dbname -N"
@@ -53,6 +84,9 @@ sed "s/METASTORE_LISTENERS/${METASTORE_LISTENERS}/" -i /etc/hive/conf/hive-site.
 [[ ! -z $ENABLE_GLUESYNC ]] && export METASTORE_PRELISTENERS="${METASTORE_PRELISTENERS},ApiaryGluePreEventListener"
 export METASTORE_PRELISTENERS=$(echo $METASTORE_PRELISTENERS|sed 's/^,//')
 sed "s/METASTORE_PRELISTENERS/${METASTORE_PRELISTENERS}/" -i /etc/hive/conf/hive-site.xml
+
+#required to debug ranger plugin, todo: send apache common logs to cloudwatch
+#export HADOOP_OPTS="$HADOOP_OPTS -Dorg.apache.commons.logging.LogFactory=org.apache.commons.logging.impl.LogFactoryImpl -Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
 
 export AUX_CLASSPATH="/usr/share/java/mariadb-connector-java.jar:/usr/share/aws/aws-java-sdk/*"
 su hive -s/bin/bash -c "/usr/lib/hive/bin/hive --service metastore --hiveconf hive.root.logger=${loglevel},console --hiveconf javax.jdo.option.ConnectionURL=jdbc:mysql://${dbhost}:3306/${dbname} --hiveconf javax.jdo.option.ConnectionUserName=${dbuser} --hiveconf javax.jdo.option.ConnectionPassword=${dbpass}"
