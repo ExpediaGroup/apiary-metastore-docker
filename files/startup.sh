f4f82c56efb06ac62beb82fbf339d6dce71e4253
Anton Kravchuk <akravchuk@expedia.com>
2020-06-05 11:40:59 -0700

    creating system db

--- files/startup.sh
+++ files/startup.sh
@@ -95,8 +95,7 @@ if [[ ! -z $KAFKA_BOOTSTRAP_SERVERS ]]; then
 fi
 
 APIARY_S3_INVENTORY_SCHEMA=s3_inventory
-APIARY_S3_LOGS_SCHEMA=s3_logs_hive
-RANGER_LOGS_SCHEMA=ranger_access_logs
+APIARY_SYSTEM_SCHEMA=system
 
 #check if database is initialized, test only from rw instances and only if DB is managed by apiary
 if [ -z $EXTERNAL_DATABASE ] && [ "$HIVE_METASTORE_ACCESS_MODE" = "readwrite" ]; then
@@ -115,12 +114,8 @@ if [ -z $EXTERNAL_DATABASE ] && [ "$HIVE_METASTORE_ACCESS_MODE" = "readwrite" ];
         else
             HIVE_APIARY_DB_NAMES="${HIVE_DB_NAMES}"
         fi
-        if [ ! -z $ENABLE_S3_LOGS ]; then
-            HIVE_APIARY_DB_NAMES="${HIVE_APIARY_DB_NAMES},${APIARY_S3_LOGS_SCHEMA}"
-        fi
-        if [ ! -z $ENABLE_RANGER_LOGS ]; then
-            HIVE_APIARY_DB_NAMES="${HIVE_APIARY_DB_NAMES},${RANGER_LOGS_SCHEMA}"
-        fi
+
+        HIVE_APIARY_DB_NAMES="${HIVE_APIARY_DB_NAMES},${APIARY_SYSTEM_SCHEMA}"
 
         AWS_ACCOUNT=`aws sts get-caller-identity|jq -r .Account`
         for HIVE_DB in `echo ${HIVE_APIARY_DB_NAMES}|tr "," "\n"`
