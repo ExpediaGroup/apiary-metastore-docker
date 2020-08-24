0a60f07a75edbfd18ee01eb6dc00a2e088d95346
Scott Barnhart <sbarnhart@expedia.com>
2020-08-24 09:16:59 -0700

    address PR comments.

--- files/update_property.py
+++ files/update_property.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+#
+# Copyright (C) 2018-2020 Expedia, Inc.
+#
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
