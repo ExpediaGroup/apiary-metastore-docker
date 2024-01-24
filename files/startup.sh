12532d70bf8c410be2041a3e5b06560095616c1e
Dhrubajyoti Sadhu <dhsadhu@expediagroup.com>
2024-01-24 14:32:12 +0000

    kubernetes exporter here: EXPORTER_OPTS and HIVE_METASTORE_HADOOP_OPTS

--- files/startup.sh
+++ files/startup.sh
@@ -100,6 +100,8 @@ if [ -n "$ENABLE_METRICS" ]; then
     if [ -n "$ECS_CONTAINER_METADATA_URI" ]; then
         echo "INSIDE - ECS_CONTAINER_METADATA_URI"
 
+        export HIVE_METASTORE_HADOOP_OPTS="$HIVE_METASTORE_HADOOP_OPTS -Djava.net.preferIPv4Stack=true -javaagent:/usr/lib/apiary/jmx_prometheus_javaagent-${EXPORTER_VERSION}.jar=8080:/etc/hive/conf/jmx-exporter.yaml"
+        export EXPORTER_OPTS="-javaagent:/usr/lib/apiary/jmx_prometheus_javaagent-${EXPORTER_VERSION}.jar=8080:/etc/hive/conf/jmx-exporter.yaml"
         export CLOUDWATCH_NAMESPACE="${INSTANCE_NAME}-metastore"
         export ECS_TASK_ID=$(wget -q -O - ${ECS_CONTAINER_METADATA_URI}/task|jq -r .TaskARN|awk -F/ '{ print $NF }')
         update_property.py hive.service.metrics.class com.expediagroup.apiary.extensions.metastore.metrics.CodahaleMetrics /etc/hive/conf/hive-site.xml
